3e9a45e02e174879ad476464e969020bef39d3b9
Muhozgu <muhozgu_aehit@students.vizja.pl>
2025-05-13 14:58:53 +0200

    Grains

---
+++ lasagna.py
@@ -0,0 +1,13 @@
+EXPECTED_BAKE_TIME = 40
+PREPARATION_TIME = 2
+
+def bake_time_remaining(actual_time):
+    return EXPECTED_BAKE_TIME - actual_time
+
+def preparation_time_in_minutes(number_of_layers):
+    return number_of_layers * PREPARATION_TIME
+
+def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
+
+print(elapsed_time_in_minutes(3, 20))
